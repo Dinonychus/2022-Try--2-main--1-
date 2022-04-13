0942f322acd8dfe59b2af23f86fefafb55843de4
Dinonychus <72425800+Dinonychus@users.noreply.github.com>
2022-04-12 19:30:08 -0700

    arcadeDrive_Maybe?

---
+++ 2022-Try--2-main/7327_command_dear_universe_why/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+public final class Constants {
+
+    //deadzone for driver controllers
+    public static final double deadzone_Limit = 0.1;
+
+    //driveTrain IDs
+    public static final int northWest_ID = 14;
+    public static final int southWest_ID = 7;
+    public static final int northEast_ID = 1;
+    public static final int southEast_ID = 3;
+
+    public static final int driverController_ID = 0;
+
+    public static final int mechanismController_ID = 1;
+
+    public final static int LEFT_STICK_Y = 1;
+
+    public final static int RIGHT_STICK_X = 4;
+
+
+
+
+
+}
---
+++ 2022-Try--2-main/7327_command_dear_universe_why/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.simulation.XboxControllerSim;
+import frc.robot.commands.arcadeDriveCMD;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.driveTrain;
+import frc.robot.subsystems.intakeSubSystem;
+import frc.robot.subsystems.shooterSubSystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+
+public class RobotContainer {
+
+
+
+  private final shooterSubSystem shooterSubSystem = new shooterSubSystem();
+  private final driveTrain driveTrain = new driveTrain();
+  private final intakeSubSystem intakeSubSystem = new intakeSubSystem();
+
+  //controllers
+  public final static XboxController controller0 = new XboxController(Constants.driverController_ID);
+  private  XboxController controller1 = new XboxController(Constants.mechanismController_ID);
+  
+
+
+  public double GetDriverRawAxis(int axis){
+    return controller0.getRawAxis(axis);
+   
+
+  }
+
+  public RobotContainer() {
+    driveTrain.setDefaultCommand(new arcadeDriveCMD(driveTrain));
+    configureButtonBindings();
+  }
+
+
+  private void configureButtonBindings() {
+
+  }
+  
+
+  public Command getAutonomousCommand() {
+    return m_autonomousCommand;
+  }
+}
